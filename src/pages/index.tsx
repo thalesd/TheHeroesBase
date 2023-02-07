ff9ea8d8c531e0fdf22c1036ccde0da4d6e81f8e
Thales Donizetti <TDonizetti@PeopleProductive.com>
2023-02-07 20:23:51 -0300

    Makes auto redirect from 404 page and changes home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto possimus consectetur aut repudiandae, iste perferendis omnis laboriosam. Dolor sapiente excepturi ratione, fugit et libero repudiandae soluta laborum, ullam facere non.</p>
         <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores officiis sed recusandae repellat, sapiente similique eum natus, quibusdam commodi, voluptatibus fugiat. Rem maxime iste eius atque at quod debitis? Repellat?</p>
         <div className={styles.btnContainer}>
-          <Link className={styles.btn} href={"/heroes"}>See Heroes List</Link>
-          <Link className={styles.btn} href={"/villains"}>See Villains List</Link>
+          <Link className={styles.btnHeroes} href={"/heroes"}>See Heroes List</Link>
+          <Link className={styles.btnVillains} href={"/villains"}>See Villains List</Link>
         </div>
       </div>
     </>
